06029a340c51cb04dffa297e0be119c01604a9c1
conal-exploits <madhavk6495@gmail.com>
2022-09-20 19:24:02 +0530

    added palindrome problem

---
+++ palindrom.js
@@ -0,0 +1,15 @@
+let str="aman"
+function reverse(string){
+let bag="";
+for (let i=string.length-1;i>=0;i--){
+bag+=string[i];
+}
+return bag;
+}
+let rev_str=reverse(str);
+
+if(str==rev_str){
+console.log("palindrome);
+}else{
+console.log("NA");
+} 
